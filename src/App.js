76e91bed0419b9a9756f8556f678b4b57a8eaab0
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-25 10:04:25 -0300

    use emotion styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
 import React from 'react'
+import { keyframes } from '@emotion/core'
+import styled from '@emotion/styled'
+
 import logo from './logo.svg'
-import './App.css'
+
+const AppLogoSpin = keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`
+
+const AppContainer = styled.div`
+  text-align: center;
+`
+const AppLogo = styled.img`
+  animation: ${AppLogoSpin} infinite 20s linear;
+  height: 40vmin;
+`
+const AppHeader = styled.header`
+  background-color: #282c34;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  font-size: calc(10px + 2vmin);
+  color: white;
+`
+const Link = styled.a`
+  color: #61dafb;
+`
 
 const App = () => (
-  <div className="App">
-    <header className="App-header">
-      <img src={logo} className="App-logo" alt="logo" />
+  <AppContainer>
+    <AppHeader className="App-header">
+      <AppLogo src={logo} className="App-logo" alt="logo" />
       <p>
         Edit
         {' '}
@@ -13,11 +45,11 @@ const App = () => (
         {' '}
 and save to reload.
       </p>
-      <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+      <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
         Learn React
-      </a>
-    </header>
-  </div>
+      </Link>
+    </AppHeader>
+  </AppContainer>
 )
 
 export default App
