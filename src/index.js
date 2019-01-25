860e8edcc88d25f2d2f7668bacf629a26c6015a8
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-25 10:55:57 -0300

    check components update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,11 @@ import EmotionProvider from './config/emotion'
 
 import * as serviceWorker from './serviceWorker'
 
+if (process.env.NODE_ENV !== 'production') {
+  const { whyDidYouUpdate } = require('why-did-you-update') // eslint-disable-line global-require
+  whyDidYouUpdate(React)
+}
+
 const providers = [EmotionProvider]
 
 const RootApp = () => [...providers, App].reduce((Comp1, Comp2) => (
