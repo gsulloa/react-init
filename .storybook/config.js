453ac87c9d2a15137ef3fc5b0faa42429c3c4f62
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-25 09:16:22 -0300

    :sparkles: chore: add storybook with default configs and addons (#2)
    * ini storybok
    
    * add addons
    
    * eslint fix and config

---
+++ .storybook/config.js
@@ -0,0 +1,9 @@
+import { configure, addDecorator } from '@storybook/react'
+import { checkA11y } from '@storybook/addon-a11y'
+
+addDecorator(checkA11y)
+function loadStories() {
+  require('../src/stories')
+}
+
+configure(loadStories, module)
