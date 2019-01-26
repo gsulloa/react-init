f7cb663b80c0154180db558c5b012d0d534b41db
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-26 12:09:29 -0300

    :wrench: chore: storybook configs and versions (#14)
    * use differents files and update to 5.0.0-alpha
    
    * using load stories dynamic

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,21 @@
+import React from 'react'
 import { configure, addDecorator } from '@storybook/react'
 import { checkA11y } from '@storybook/addon-a11y'
+import withEmotionProvider from '../src/config/emotion'
+import withI18nProvider from '../src/config/i18n'
+import withRouter from '../src/config/router'
+import pipe from '../src/utils/pipe'
 
 addDecorator(checkA11y)
+addDecorator(story => {
+  const providers = [withEmotionProvider, withI18nProvider, withRouter].reverse()
+  const WithProviders = pipe(...providers)(story)
+  return <WithProviders />
+})
+const req = require.context('../src/stories', true, /\.js$/)
+
 function loadStories() {
-  require('../src/stories')
+  req.keys().forEach(filename => req(filename))
 }
 
 configure(loadStories, module)
