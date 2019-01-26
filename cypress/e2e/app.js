285931ff44da7c67220a2d9795eede17096c02c5
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-26 04:11:07 -0300

    add cypress testing library

--- cypress/e2e/app.js
+++ cypress/e2e/app.js
@@ -1,7 +1,7 @@
 describe('general app', () => {
   it('open the app', () => {
     cy.visit('http://localhost:8080')
-      .get('.css-e9t3bf-Button')
+      .getByText(/^Emotion/)
       .click()
       .get('p')
       .should('have.text', 'Edit src/Home.js and save to reload.')
