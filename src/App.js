0eef4f03f1028cb703315a79dce0ae861280e712
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-26 01:05:58 -0300

    :sparkles: chore: using react router (#10)
    * using router in app
    
    * using config file for routes

--- src/App.js
+++ src/App.js
@@ -1,66 +1,13 @@
 import React from 'react'
-import { keyframes } from '@emotion/core'
-import styled from '@emotion/styled'
-import logo from './logo.svg'
-import Button from './components/button'
-
-const AppLogoSpin = keyframes`
-  from {
-    transform: rotate(0deg);
-  }
-  to {
-    transform: rotate(360deg);
-  }
-`
-
-const AppContainer = styled.div`
-  text-align: center;
-`
-const AppLogo = styled.img`
-  animation: ${AppLogoSpin} infinite 20s linear;
-  height: 40vmin;
-`
-const AppHeader = styled.header`
-  background-color: #282c34;
-  min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  font-size: calc(10px + 2vmin);
-  color: white;
-`
-const Link = styled.a`
-  color: #61dafb;
-`
+import { Switch, Route } from 'react-router-dom'
+import Home from './screens/Home'
+import routes from './config/router/routes'
 
 const App = () => (
-  <AppContainer>
-    <AppHeader className="App-header">
-      <AppLogo src={logo} className="App-logo" alt="logo" />
-      <p>
-        Edit
-        {' '}
-        <code>src/App.js</code>
-        {' '}
-and save to reload.
-      </p>
-      <Link href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-        Learn React
-      </Link>
-      <Button
-        onClick={() => {
-          throw new Error('testing sentry with logrocket')
-        }}
-      >
-        Emotion
-        {' '}
-        <code>css</code>
-        {' '}
-example
-      </Button>
-    </AppHeader>
-  </AppContainer>
+  <Switch>
+    <Route component={Home} exact path={routes.home} />
+    <Route render={() => <div>not found</div>} />
+  </Switch>
 )
 
 export default App
