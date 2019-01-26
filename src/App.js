b9d0454580b3755c805c775080ce635f80182bf7
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-26 01:04:24 -0300

    using config file for routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Home from './screens/Home'
+import routes from './config/router/routes'
 
 const App = () => (
   <Switch>
-    <Route component={Home} exact path="/" />
+    <Route component={Home} exact path={routes.home} />
     <Route render={() => <div>not found</div>} />
   </Switch>
 )
