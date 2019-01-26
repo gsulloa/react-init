569c270002a26710e1d1385565c203d46b3ca258
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-26 00:24:13 -0300

    rename i18n hoc

--- src/config/i18n/index.js
+++ src/config/i18n/index.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { I18nextProvider } from 'react-i18next'
 import instance from './instance'
 
-const HOC = WrappedComponent => props => (
+const withI18nProvider = WrappedComponent => props => (
   <I18nextProvider i18n={instance}>
     <WrappedComponent {...props} />
   </I18nextProvider>
 )
 
-export default HOC
+export default withI18nProvider
