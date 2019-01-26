fef3629cd770a697b1d7a9e07b88b3862f39baf2
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-26 03:38:54 -0300

    tests for not found

--- src/__tests__/App.js
+++ src/__tests__/App.js
@@ -11,7 +11,17 @@ it('renders welcome message', () => {
   expect(getByText('Learn React')).toBeInTheDocument()
 })
 
-it('match snapshot', () => {
+it('match snapshot at home', () => {
   const { container } = render(<App />)
+  expect(container.firstChild).toMatchSnapshot()
+})
+
+it('renders welcome message', () => {
+  const { getByText } = render(<App />, { initialEntries: ['/not/found/route'] })
+  expect(getByText('not found')).toBeInTheDocument()
+})
+
+it('match snapshot at not found', () => {
+  const { container } = render(<App />, { initialEntries: ['/custom/route'] })
   expect(container.firstChild).toMatchSnapshot()
 })
