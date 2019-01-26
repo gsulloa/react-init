893d549d1a90c60e0a1c1923a0c39fb946d4fe62
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-26 12:42:23 -0300

    add redux logger

--- src/config/rematch/store.js
+++ src/config/rematch/store.js
@@ -1,8 +1,17 @@
 import { init } from '@rematch/core'
+import reduxLogger from 'redux-logger'
+import { SHOULD_LOG } from '..'
 
 const createStore = ({ models = {} } = {}) => {
+  const middlewares = []
+  if (SHOULD_LOG) {
+    middlewares.push(reduxLogger)
+  }
   const store = init({
     models,
+    redux: {
+      middlewares,
+    },
   })
   return store
 }
