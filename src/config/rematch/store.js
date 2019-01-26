41f3d4961396a2e6c8ed90ae365268e8e9aee752
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-26 13:01:46 -0300

    loading plugin

--- src/config/rematch/store.js
+++ src/config/rematch/store.js
@@ -1,6 +1,7 @@
 import { init } from '@rematch/core'
 import reduxLogger from 'redux-logger'
 import createRematchPersist from '@rematch/persist'
+import createLoadingPlugin from '@rematch/loading'
 import { SHOULD_LOG } from '..'
 
 const createStore = ({ models = {} } = {}) => {
@@ -12,13 +13,15 @@ const createStore = ({ models = {} } = {}) => {
   const persistPlugin = createRematchPersist({
     throttle: 1000,
     version: 1,
+    blacklist: ['loading'],
   })
+  const loadingPlugin = createLoadingPlugin()
   const store = init({
     models,
     redux: {
       middlewares,
     },
-    plugins: [persistPlugin],
+    plugins: [persistPlugin, loadingPlugin],
   })
   return store
 }
--- src/screens/Rematch/index.js
+++ src/screens/Rematch/index.js
@@ -2,27 +2,36 @@ import React from 'react'
 import t from 'prop-types'
 import { connect } from 'react-redux'
 
-const Count = ({ count, increment, incrementAsync }) => (
+const Count = ({
+  count, increment, incrementAsync, loading, allLoadingObject,
+}) => (
   <div>
     The count is
     {' '}
     {count}
-    <button type="button" onClick={increment}>
+    <button type="button" onClick={increment} disabled={loading}>
       increment
     </button>
-    <button type="button" onClick={incrementAsync}>
+    <button type="button" onClick={incrementAsync} disabled={loading}>
       incrementAsync
     </button>
+    <div>{JSON.stringify(allLoadingObject, null, 2)}</div>
   </div>
 )
 Count.propTypes = {
   count: t.number.isRequired,
+  loading: t.bool.isRequired,
   increment: t.func.isRequired,
   incrementAsync: t.func.isRequired,
+  allLoadingObject: t.shape({
+    globals: t.bool.isRequired,
+  }).isRequired,
 }
 
 const mapState = state => ({
   count: state.count,
+  loading: state.loading.models.count,
+  allLoadingObject: state.loading,
 })
 
 const mapDispatch = ({ count: { increment, incrementAsync } }) => ({
