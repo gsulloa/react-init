ed7284f3a451ec86c75bb4431b95959a729af531
Gabriel Ulloa <gsulloa@uc.cl>
2019-01-25 23:57:05 -0300

    hoc instead components

--- src/config/i18n/index.js
+++ src/config/i18n/index.js
@@ -3,9 +3,10 @@ import t from 'prop-types'
 import { I18nextProvider } from 'react-i18next'
 import instance from './instance'
 
-const Provider = ({ children }) => <I18nextProvider i18n={instance}>{children}</I18nextProvider>
-Provider.propTypes = {
-  children: t.element.isRequired,
-}
+const HOC = WrappedComponent => props => (
+  <I18nextProvider i18n={instance}>
+    <WrappedComponent {...props} />
+  </I18nextProvider>
+)
 
-export default Provider
+export default HOC
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App'
 
 import EmotionProvider from './config/emotion'
+import withI18nProvider from './config/i18n'
 
 import * as serviceWorker from './serviceWorker'
 
@@ -17,12 +18,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const providers = [EmotionProvider]
 
-const RootApp = () => [...providers, App].reduce((Comp1, Comp2) => (
-  <Comp1>
-    <Comp2 />
-  </Comp1>
-))
-
+const RootApp = withI18nProvider(App)
 ReactDOM.render(<RootApp />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
